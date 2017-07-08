10e1f91a9dda7215cd8fb5611ac8f374f529cf59
Andrew Carr <andrewpcarr@gmail.com>
2017-07-08 01:46:13 -0500

    Adding final project files

---
+++ controllers/api-routes.js
@@ -0,0 +1,30 @@
+var db = require("../models");
+
+//these modify the db (API)
+module.exports = function(app) {
+  //updateOne
+  app.put('/:id', function(req, res) {
+    //updateOne becomes update
+    db.burger.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(result) {
+      res.redirect("/");
+    });
+  });
+
+  //add
+  app.post('/', function(req, res) {
+    //console.log(req.body);
+    //create method becomes the old insertOne
+    db.burger.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }).then(function(result) {
+      res.redirect("/");
+    });
+  });
+};
